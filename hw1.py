14b86b6234c900b2edc95b316e2abd234c49ab48
Luchezar03 <yurapyshuk@gmail.com>
2021-03-29 14:36:37 +0300

    start git

---
+++ hw1.py
@@ -0,0 +1,20 @@
+import re
+
+input_file = open('hw')
+regex = '([(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (\d+)'
+code_resoponse_idx = 3
+ip_idx = 0
+code_resoponse = '404'
+
+result = {}
+for line in input_file:
+    log = re.match(regex, line).groups()
+    if log[code_resoponse_idx] == code_resoponse:
+        try:
+            result[log[ip_idx]] = result[log[ip_idx]] + 1
+        except:
+            result[log[ip_idx]] = 1
+
+print(log)
+
+input_file.close()
